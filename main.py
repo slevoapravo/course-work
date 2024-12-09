dd2c24a707ce1803f9eff6ab824f5ef530fc122c
Your Name <you@example.com>
2024-12-09 17:34:18 +0300

    fixed4

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from src.services import investment_bank, str_date_service, transactions
 from src.utils import currency, greeting, number_cards, read_file, stock_prices, to_file, top_transactions
 from src.views import main, str_begin_date
 
-
-
 if __name__ == "__main__":
-    str_begin_date = "2023-01-01"
+    str_begin_date = "2025-01-01"
     print(main(str_begin_date))
